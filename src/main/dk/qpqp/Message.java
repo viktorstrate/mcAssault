782c0484279fc22b49a2d085d1071f1b69f74d31
niek1e <prinscou@gmail.com>
2014-11-18 15:40:42 +0100

    Fixed bug.

--- src/main/dk/qpqp/Message.java
+++ src/main/dk/qpqp/Message.java
@@ -5,9 +5,7 @@ import org.bukkit.plugin.Plugin;
 
 public class Message {
 	
-	public static String pluginName = "McAssault";
-	
-	private static String prefix = "[McAssault]";
+	private static String prefix = "McAssault";
 	
 	public static void playerMessage(String message, Player player, Plugin plugin){
 		
